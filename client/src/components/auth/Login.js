2b2912caa95b1c71cbfb9f19b50511ae55fed785
Andrei B <ruby.project15@gmail.com>
2021-05-14 12:09:00 +0300

    auth setup complete

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Login = () => {
+const Login = props => {
+    const alertContext = useContext(AlertContext)
+    const authContext = useContext(AuthContext)
+
+    const {setAlert} = alertContext;
+    const {login, error, clearErrors, isAuthenticated} = authContext;
+
+    useEffect(() => {
+        if(isAuthenticated){
+            props.history.push('/')
+        }
+        if(error === 'Invalid Credentials'){
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+        // eslint-disable-next-line
+    },[error, isAuthenticated, props.history])
+
     const [user, setUser] = useState({
         email: '',
         password: ''
@@ -11,7 +30,14 @@ const Login = () => {
     const onChange = e => setUser({...user, [e.target.name]: e.target.value })
     const onSubmit = e => {
         e.preventDefault();
-        console.log('log')
+        if (email ==='' || password === '') {
+            setAlert('Please fill in all fields', 'danger');
+        } else {
+            login({
+                email,
+                password
+            });
+        }
     }
     return (
         <div className = 'form-container'>
@@ -21,11 +47,11 @@ const Login = () => {
             <form onSubmit = {onSubmit}>
                 <div className='form-group'>
                     <label htmlFor='email'>Email</label>
-                    <input type='email' name='email' value={email} onChange={onChange}/>
+                    <input type='email' required name='email' value={email} onChange={onChange}/>
                 </div>
                 <div className='form-group'>
                     <label htmlFor='password'>Password</label>
-                    <input type='password' name='password' value={password} onChange={onChange}/>
+                    <input type='password' required name='password' value={password} onChange={onChange}/>
                 </div>
                 <input type='submit' value='Login' className='btn btn-primary btn-block'></input>
             </form>
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -88,7 +88,7 @@ const AuthState = props => {
     }
 
     // logout
-    const logout = () => console.log('logout user')
+    const logout = () => dispatch({type: LOGOUT})
 
     // clear errors
     const clearErrors = () => dispatch({ type: CLEAR_ERRORS })
